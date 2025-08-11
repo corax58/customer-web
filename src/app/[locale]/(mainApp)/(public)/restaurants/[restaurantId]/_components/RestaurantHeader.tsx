799b1ebd331238eeb788df524decbb5d7c646a8c
Abubeker Abduljelil <abubekera87@gmail.com>
2025-08-11 23:13:16 +0300

    feat: add deliverd by to restaurant header

--- src/app/[locale]/(mainApp)/(public)/restaurants/[restaurantId]/_components/RestaurantHeader.tsx
+++ src/app/[locale]/(mainApp)/(public)/restaurants/[restaurantId]/_components/RestaurantHeader.tsx
@@ -28,6 +28,18 @@ const RestaurantHeader = ({
           )}
         </div>
         <div className="flex items-stretch gap-3 lg:items-center lg:gap-7">
+          <div className="flex h-full flex-col items-center justify-start max-lg:gap-2 lg:items-end">
+            <p className="text-muted-foreground text-sm max-lg:text-center max-lg:text-xs">
+              <span>Delivered by</span>
+            </p>
+            <p className="text-primary text-center font-medium max-lg:text-sm">
+              Time Delivery
+            </p>
+          </div>
+          <div className="flex h-16 w-fit max-w-px flex-1 items-center">
+            <div className="bg-border h-8 w-px" />
+          </div>
+
           {restaurant.delivery_info.delivery_time_minutes && (
             <>
               <div className="flex h-full flex-col items-center justify-start max-lg:gap-2 lg:items-end">
@@ -35,7 +47,7 @@ const RestaurantHeader = ({
                   <span className="max-sm:hidden">
                     Earliest Time of arrival
                   </span>
-                  <span className="sm:hidden">Est. time of arrival</span>
+                  <span className="sm:hidden">Delivery Time</span>
                 </p>
                 <p className="font-medium max-lg:text-sm">
                   {formatTimeHM(parseFloat(restaurant.estimated_delivery_time))}
